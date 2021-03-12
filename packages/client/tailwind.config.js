d968c8664cdc8a0fbb98e11d7392163fbeab2866
Pdom <ahb0327@naver.com>
2021-03-12 17:20:26 +0900

    Update tailwind config

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,11 +1,42 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        2: '2 2 0%',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      primary: colors.indigo,
+      danger: colors.rose,
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    filter: {
+      none: 'none',
+      grayscale: 'grayscale(1)',
+      invert: 'invert(1)',
+      sepia: 'sepia(1)',
+    },
+    backdropFilter: {
+      none: 'none',
+      blur: 'blur(20px)',
+    },
   },
   variants: {
+    filter: ['responsive'],
+    backdropFilter: ['responsive'],
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-filters')],
 };
