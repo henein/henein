2a99ba5f00f8c19a4fdb7a35aed578756fc84095
Pdom <ahb0327@naver.com>
2021-03-15 06:46:51 +0900

    Create @henein/server

---
+++ packages/server/src/index.ts
@@ -0,0 +1,34 @@
+import Koa from 'koa';
+import serverless from 'serverless-http';
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello, World!';
+});
+
+app.listen(3000);
+
+export const handler = serverless(app);
+
+// import "reflect-metadata";
+// import {createConnection} from "typeorm";
+// import {User} from "./entity/User";
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
---
+++ packages/server/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  externals: [nodeExternals()],
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+};
