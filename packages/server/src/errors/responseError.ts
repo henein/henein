7c3e1f3065c462d94834be9812d074550ff733f6
Pdom <ahb0327@naver.com>
2021-03-31 01:47:15 +0900

    Update responseError.ts

--- packages/server/src/errors/responseError.ts
+++ packages/server/src/errors/responseError.ts
@@ -1,14 +1,15 @@
 export class ResponseError extends Error {
-    status: number;
-  
-    constructor(status: number, message?: string) {
-      super(message);
-  
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, ResponseError);
-      }
-  
-      this.status = status;
+  status: number;
+  expose: boolean;
+
+  constructor(status: number, message?: string) {
+    super(message);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ResponseError);
     }
+
+    this.status = status;
+    this.expose = true;
   }
-  
+}
