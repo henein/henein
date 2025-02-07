30aa0de95d866eece464d4aef0a90e681371f23f
donggyun <donggyunkim12@gmail.com>
2025-02-08 01:10:31 +0900

    feat(UserPage): 페이지 별 라우트에 따른 에러처리 구현
    - uid에 해당하는 유저가 없다면 not-found.tsx 사용
    - 이외의 supabase 에러라면 error.tsx 사용
    - /user 라우트 로그인된 유저라면 본인 페이지로 redirect 구현
    - users 테이블 이용한 데이터 페칭 구현
    
    Resolves: #5

--- src/app/user/[uid]/page.tsx
+++ src/app/user/[uid]/page.tsx
@@ -2,6 +2,8 @@ import Contents from '../_components/Contents';
 import ContentsNavigator from '../_components/ContentsNavigator';
 import Pagenate from '../_components/Pagenate';
 import Profile from '../_components/Profile';
+import { createClient } from '@/utils/supabase/server';
+import { notFound } from 'next/navigation';
 import React, { use } from 'react';
 
 interface Props {
@@ -12,8 +14,9 @@ interface Props {
 const Userpage = ({ params, searchParams }: Props) => {
   const { uid } = use(params);
   const { type } = use(searchParams);
+  const data = use(fetchUser(uid));
+  console.log(data);
 
-  console.log(uid);
   return (
     <div className="m-[0_auto] flex w-full max-w-[1024px] flex-col">
       <h2 className="mb-6 mt-6 text-left text-3xl font-bold">유저 페이지</h2>
@@ -26,3 +29,20 @@ const Userpage = ({ params, searchParams }: Props) => {
 };
 
 export default Userpage;
+
+const fetchUser = async (uid: string) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('id', uid);
+
+  if (error) {
+    // 해당되는 데이터를 못찾았다면, 404로 리다이렉트
+    if (error.code === '22P02') return notFound();
+    // 이외의 에러라면 throw Error
+    throw new Error(error.message);
+  }
+
+  return data;
+};
