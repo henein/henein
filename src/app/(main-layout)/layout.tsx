65e93e96d6d02f88273b659159657db177acd41f
donggyun <donggyunkim12@gmail.com>
2025-02-10 17:53:23 +0900

    feat: tanstack-query 초기 설정

--- src/app/(main-layout)/layout.tsx
+++ src/app/(main-layout)/layout.tsx
@@ -1,9 +1,14 @@
-import { MamudaeLayout } from "@/components";
+import { MamudaeLayout } from '@/components';
+import QueryProvider from '@/utils/query/QueryProvider';
 
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <MamudaeLayout>{children}</MamudaeLayout>;
+  return (
+    <QueryProvider>
+      <MamudaeLayout>{children}</MamudaeLayout>
+    </QueryProvider>
+  );
 }
