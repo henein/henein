68edf6f9e8c4fb428008a1215735fcf60de9d020
Pdom <pdom0327@gmail.com>
2025-02-15 11:09:47 +0900

    feat(BossPage): 모바일 대응

--- src/components/mamudae-header.tsx
+++ src/components/mamudae-header.tsx
@@ -28,9 +28,9 @@ export const MamudaeHeader = () => {
               {/* <Link href="/mamudae/record"> */}
               <NavigationItem isWIP>기록</NavigationItem>
               {/* </Link> */}
-              <Link href="/community">
-                <NavigationItem>커뮤니티</NavigationItem>
-              </Link>
+              {/* <Link href="/community"> */}
+                <NavigationItem isWIP>커뮤니티</NavigationItem>
+              {/* </Link> */}
             </div>
           </div>
           <div className="flex gap-4">
