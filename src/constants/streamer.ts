c3d8b3cad18613c69ce6fe5000d22b1d3ea0c7a3
Pdom <pdom0327@gmail.com>
2025-02-15 10:57:05 +0900

    feat: 지명님 추가

--- src/constants/streamer.ts
+++ src/constants/streamer.ts
@@ -12,6 +12,7 @@ export enum StreamerId {
   NAMJIO = 'namjio',
   NUSEUNYANG = 'nuseunyang',
   UDEONG = 'udeong',
+  JIMYEONG = 'jimyeong'
 }
 
 export type StreamerPlatform = 'soop' | 'youtube' | 'chzzk';
@@ -202,6 +203,20 @@ export const Streamers: Streamer[] = [
       {
         platform: 'chzzk',
         link: 'https://chzzk.naver.com/4cc2031a4597b6dd2d75c13fd375e433'
+      }
+    ],
+  },
+  {
+    id: StreamerId.JIMYEONG,
+    nickname: '지명',
+    links: [
+      {
+        platform: 'youtube',
+        link: 'https://www.youtube.com/@Ji-Myeong1214'
+      },
+      {
+        platform: 'chzzk',
+        link: 'https://chzzk.naver.com/7790fbff6c378aea5096be800307cfcb'
       }
     ],
   },
