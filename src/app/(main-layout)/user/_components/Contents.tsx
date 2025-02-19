dea8eb1f9b1326ed749272d9bf1b4a6165e39aca
donggyun <donggyunkim12@gmail.com>
2025-02-19 23:30:11 +0900

    refactor(user): 유저 페이지 Contents 리팩토링

--- src/app/(main-layout)/user/_components/Contents.tsx
+++ src/app/(main-layout)/user/_components/Contents.tsx
@@ -1,87 +1,33 @@
+import Pagenation from './Pagenation';
+import PostList from './PostList';
 import CharacterContents from './character';
-import PostItem from '@/components/post-item';
-import { getTimeDifference } from '@/utils/time';
-import { PrismaClient } from '@prisma/client';
+import { fetchPostsFromType } from '@/actions/user-posts-fetch';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 interface Props {
   uid: string;
   type: string;
-  page: string | string[] | undefined;
+  page: number;
   isMyProfile: boolean;
 }
 
-const MAX_POST_COUNT = 20;
+const Contents = async ({ uid, type, isMyProfile, page }: Props) => {
+  const posts = await fetchPostsFromType(uid, type);
 
-const Contents = async ({ uid, type, isMyProfile }: Props) => {
-  const prisma = new PrismaClient();
+  if (type === 'character' && isMyProfile) {
+    return <CharacterContents uid={uid} />;
+  }
 
-  const posts = await prisma.posts.findMany({
-    include: {
-      categories: true,
-      users: { include: { profiles: true } },
-    },
-    where: {
-      author_id: uid,
-      deleted_at: null,
-    },
-    orderBy: {
-      created_at: 'desc',
-    },
-    // take: MAX_POST_COUNT,
-    // skip: (page - 1) * MAX_POST_COUNT,
-  });
-
-  if (type === 'post')
+  if (type === 'post' || type === 'comment')
     return (
-      <>
-        <div className="flex h-full w-full flex-col justify-between">
-          <div className="divide-default flex w-full flex-auto flex-col divide-y">
-            {posts.map((post) => (
-              <PostItem
-                key={post.id}
-                category={post.categories.name}
-                categoryId={post.categories.id}
-                id={post.id}
-                title={post.title}
-                text={''}
-                userName={post.users.profiles?.nickname ?? ''}
-                createTime={getTimeDifference(post.created_at.toISOString())}
-                views={0}
-                commentNum={0}
-                recommendNum={0}
-              />
-            ))}
-          </div>
-          {/* <div className="flex w-full justify-center gap-0.5">
-            <PaginationLink href={getHref(category, 1)}>
-              <span className="icon icon-20">keyboard_arrow_left</span>
-            </PaginationLink>
-            {getShowPages(page, pageCount).map((showPage) => (
-              <PaginationLink
-                key={showPage}
-                href={getHref(category, showPage)}
-                isSelected={showPage === page}
-              >
-                {showPage}
-              </PaginationLink>
-            ))}
-            <PaginationLink href={getHref(category, 11)}>
-              <span className="icon icon-20">keyboard_arrow_right</span>
-            </PaginationLink>
-          </div> */}
-        </div>
-      </>
+      <div className="flex h-full w-full flex-col justify-between">
+        <PostList posts={posts} type={type} />
+        <Pagenation uid={uid} page={page} totalElement={posts.length} />
+      </div>
     );
 
-  if (type === 'character' && isMyProfile)
-    return <CharacterContents uid={uid} />;
-
-  return (
-    <div className="flex h-full min-h-[300px] w-full items-center justify-center">
-      <h2 className="text-xl font-bold">해당하는 데이터가 없습니다.</h2>
-    </div>
-  );
+  return notFound();
 };
 
 export default Contents;
---
+++ src/app/(main-layout)/user/_components/PostList.tsx
@@ -0,0 +1,35 @@
+import PostItem from '@/components/post-item';
+import { getTimeDifference } from '@/utils/time';
+import React from 'react';
+
+const PostList = ({ posts, type }: { posts: any[]; type: string }) => {
+  return (
+    <div className="divide-default flex w-full flex-auto flex-col divide-y">
+      {posts.length ? (
+        posts.map((post) => (
+          <PostItem
+            key={post.id}
+            category={post.categories.name}
+            categoryId={post.categories.id}
+            id={post.id}
+            title={post.title}
+            text={''}
+            userName={post.users.profiles?.nickname ?? ''}
+            createTime={getTimeDifference(post.created_at.toISOString())}
+            views={0}
+            commentNum={0}
+            recommendNum={0}
+          />
+        ))
+      ) : (
+        <div className="flex h-full min-h-[300px] w-full flex-col items-center justify-center gap-7">
+          <h2 className="text-xl font-bold">
+            작성된 {type === 'post' ? '게시글' : '댓글'}이 없습니다.
+          </h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PostList;
