d1806bafb0da8e3480360fc5cde4920938c76a2c
donggyun <donggyunkim12@gmail.com>
2025-02-22 21:10:23 +0900

    fix: next config 보안을 위한 환경변수 NEXT_PUBLIC 제거

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      `${process.env.NEXT_PUBLIC_NEXON_API_URL}`,
-      `${process.env.NEXT_PUBLIC_STORAGE_URL}`,
-    ],
+    domains: ['open.api.nexon.com', `${process.env.STORAGE_URL}`],
   },
 };
 
