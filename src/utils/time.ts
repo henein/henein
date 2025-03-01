31015ce720e4b21eae999af88c6fd3a175a65eca
Pdom <pdom0327@gmail.com>
2025-03-01 11:57:43 +0900

    fix: timezone

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -24,7 +24,7 @@ export const getTimeDifference = (createTime: string) => {
 
 export const getTimeString = (isoString: string) => {
   return DateTime.fromISO(isoString, {
-    zone: 'utc',
+    zone: 'KST',
   })
     .setZone(DateTime.local().zoneName)
     .toFormat('yyLLdd HH:mm');
