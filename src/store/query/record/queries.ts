5e729a8d1ba401d76b80cec37756d10db0f9635f
donggyun <donggyunkim12@gmail.com>
2025-03-02 13:58:45 +0900

    fix(record): 빌드 에러에 따른 fetchLogs 수정

--- src/store/query/record/queries.ts
+++ src/store/query/record/queries.ts
@@ -1,13 +1,33 @@
 import { keepPreviousData, queryOptions } from "@tanstack/react-query";
 
-export const fetchLogs = async (chracterIds: (string | null)[]) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/mamudae/record?characterIds=${
-      chracterIds.join(",")
-    }`,
+export const fetchLogs = async (characterIds: (string | null)[]) => {
+  // id가 없을 경우 기본 빈 데이터 반환
+  const validCharacterIds = characterIds.filter((id): id is string =>
+    id !== null
   );
 
-  return res.json();
+  if (validCharacterIds.length === 0) {
+    return { logs: [] };
+  }
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/mamudae/record?characterIds=${
+        validCharacterIds.join(",")
+      }`,
+      // { cache: "no-store" },
+    );
+
+    if (!res.ok) {
+      console.error(`API 요청 실패: ${res.status} ${res.statusText}`);
+      return { logs: [] };
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("API 요청 중 오류 발생:", error);
+    return { logs: [] };
+  }
 };
 
 export const recordQueries = {
