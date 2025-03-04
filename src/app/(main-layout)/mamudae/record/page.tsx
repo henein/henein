4f40f6b344ec7fec029ce1b858783fb67c1604d6
donggyun <donggyunkim12@gmail.com>
2025-03-04 23:43:08 +0900

    feat(record): Chart Suspense 설정

--- src/app/(main-layout)/mamudae/record/page.tsx
+++ src/app/(main-layout)/mamudae/record/page.tsx
@@ -1,15 +1,23 @@
 import Chart from './_components/chart';
 import Team from './_components/team';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 const RecordPage = () => {
   return (
     <div className="mx-auto my-auto flex h-full w-full max-w-[1024px] flex-col gap-8">
-      <Chart />
-      <div className="flex flex-wrap justify-around gap-y-4">
-        <Team type="STAN" />
-        <Team type="MAYA" />
-      </div>
+      <Suspense
+        fallback={
+          <div className="flex justify-center">
+            <h1 className="text-white-900">Loading</h1>
+          </div>
+        }
+      >
+        <Chart />
+        <div className="flex flex-wrap justify-around gap-y-4">
+          <Team type="STAN" />
+          <Team type="MAYA" />
+        </div>
+      </Suspense>
     </div>
   );
 };
