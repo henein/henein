a5ef6cc4e582a6191d1a2530f54d3f218ae6ef63
donggyun <donggyunkim12@gmail.com>
2025-03-08 18:00:42 +0900

    feat(record): 기록 페이지 초기 lazy 로딩 적용 및 Suspense 스켈레톤 구현 #39

--- src/app/(main-layout)/mamudae/record/page.tsx
+++ src/app/(main-layout)/mamudae/record/page.tsx
@@ -1,25 +1,51 @@
-import Chart from './_components/chart';
 import Team from './_components/team';
-import React, { Suspense } from 'react';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/shadcnUI/card';
+import { Skeleton } from '@/components/shadcnUI/skeleton';
+import React, { lazy, Suspense } from 'react';
+
+const Chart = lazy(() => import('./_components/chart'));
 
 const RecordPage = () => {
   return (
     <div className="mx-auto my-auto flex h-full w-full max-w-[1024px] flex-col gap-8">
-      <Suspense
-        fallback={
-          <div className="flex justify-center">
-            <h1 className="text-white-900">Loading</h1>
-          </div>
-        }
-      >
+      <Suspense fallback={<SkeletonFallbackUI />}>
         <Chart />
-        <div className="flex flex-wrap justify-around gap-y-4">
-          <Team type="STAN" />
-          <Team type="MAYA" />
-        </div>
       </Suspense>
+      <div className="flex flex-wrap justify-around gap-y-4">
+        <Team type="STAN" />
+        <Team type="MAYA" />
+      </div>
     </div>
   );
 };
 
 export default RecordPage;
+
+const SkeletonFallbackUI = () => {
+  return (
+    <Card className="w-full pt-0">
+      <CardHeader>
+        <div className="flex w-full gap-3 border-b">
+          <Skeleton className="h-[76px] w-1/2 rounded-none" />
+          <Skeleton className="h-[76px] w-1/2 rounded-none" />
+        </div>
+        <CardTitle className="my-3">
+          <Skeleton className="h-6 w-[120px]" />
+        </CardTitle>
+
+        <div className="flex justify-end">
+          <Skeleton className="h-8 w-[230px]" />
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <Skeleton className="h-[350px] w-full" />
+      </CardContent>
+    </Card>
+  );
+};
